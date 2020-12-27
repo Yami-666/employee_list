265024507a10b7838e6634797d7adda385e1316c
Yami-666 <trishin.m.2002@gmail.com>
2020-12-27 15:39:16 +0300

    finally commit

--- employee_register/forms.py
+++ employee_register/forms.py
@@ -1,12 +1,17 @@
 from django.forms import ModelForm
 from .models import Employee, Passport, Statement
+from django import forms
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 
 class PassportForm(ModelForm):
 
     class Meta:
         model = Passport
         fields = ['fullname', 'serial_number', 'address', 'issed', 'code_subdivision', 'date_of_issed']
-
+        widgets = {'date_of_issed' : DateInput()}
 
 
 class EmployeeForm(ModelForm):
@@ -16,9 +21,10 @@ class EmployeeForm(ModelForm):
         fields = ['url_photo', 'phone', 'position', 'unit']
 
 
-class StatementForm(ModelForm):
-
+class StatementForm(forms.ModelForm):
     class Meta:
         model = Statement
         fields = ['date_work', 'isAttended', 'salary']
+        widgets = {'date_work' : DateInput()}
 
+    
